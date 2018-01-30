6beec3114dc652af46b66b2b85c9317a373abda9
Takahiro Naito <jem90ham@gmail.com>
2018-01-31 07:49:56 +0900

    Create template.rb

---
+++ template.rb
@@ -0,0 +1,17 @@
+gem_group :development, :test do
+  gem 'rspec-rails'
+  gem 'factory_bot'
+end
+
+gem_group :development do
+  gem 'letter_opener'
+end
+
+after_bundle do
+  git :init
+  git add: "."
+  git commit: "-m 'initial commit!'"
+  generate("rspec:install")
+  git add: "."
+  git commit: "-m 'Setup RSpec'"
+end
